e399e48644805ea112738440860dc1efecb8d0a1
Sonia-Liu <2998523379@qq.com>
2015-12-24 14:14:33 +0800

    简历页面图片注释
    简历页面图片注释

--- jianli.php
+++ jianli.php
@@ -31,13 +31,13 @@ if(!empty($_FILES) && $_FILES['fil']['error'] == 0) {
 	
 	$flag = move_uploaded_file($_FILES['fil']['tmp_name'],  dirname(__FILE__).'/data/upload'.$des);
 	if($flag){
-	    /*echo "<script>alert('上传成功');</script>";
-	    echo "<script>window.location.href="/";</script>";*/
-	    echo 'ok';
+	    echo "<script>alert('上传成功');</script>";
+	    echo "<script>window.location.href="/";</script>";
+	    //echo 'ok';
 	}else{
-	    /*echo "<script>alert('上传失败');</script>";
-	    echo "<script>window.location.href="/";</script>";*/
-	    echo 'fail';
+	    echo "<script>alert('上传失败');</script>";
+	    echo "<script>window.location.href="/";</script>";
+	    //echo 'fail';
 	}
 
 }
