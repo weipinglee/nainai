a268a60efb921e247f2662a56b2fe2fd6ace6e82
weiping <weiping.lee@163.com>
2016-02-04 18:02:03 +0800

    支付时可以选择地址

--- theme/guanwang/images/order.js
+++ theme/guanwang/images/order.js
@@ -1,5 +1,13 @@
 // JavaScript Document
+//初始化地域联动
+var area_url = 'get_area.php';
+template.compile("areaTemplate",areaTemplate);
+createAreaSelect('province',0,'',area_url);
 $(function(){
+  //初始化地区
+
+
+  //将表单内容写入父窗口form，进行提交
   $('#sub').on('click',function(){
     var formObj = $('#dingdanqueren');
 
@@ -8,5 +16,24 @@ $(function(){
     formParent.append(formObj);
     formParent.submit();
   })
+
+  //发票信息切换
+  jQuery.jqtab = function(tabtit,tab_conbox,shijian) {
+    $(tab_conbox).find("li").hide();
+    $(tabtit).find("li:first").addClass("thistab").show();
+    $(tab_conbox).find("li:first").show();
+
+    $(tabtit).find("li").bind(shijian,function(){
+      $(this).addClass("thistab").siblings("li").removeClass("thistab");
+      var activeindex = $(tabtit).find("li").index(this);
+      $(tab_conbox).children().eq(activeindex).show().siblings().hide();
+      return false;
+    });
+
+  };
+  /*调用方法如下：*/
+  $.jqtab("#tabs","#tab_conbox","click");
+
+  $.jqtab("#tabs2","#tab_conbox2","mouseenter");
 })
 
