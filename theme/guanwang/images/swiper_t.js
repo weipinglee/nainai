42eb7fa0b2f8b7241f45407b1a775eafe5420d2d
ducong123 <709558813@qq.com>
2019-01-25 14:54:04 +0800

    修改视频点击js

--- theme/guanwang/images/swiper_t.js
+++ theme/guanwang/images/swiper_t.js
@@ -11,26 +11,23 @@
           //首页视频专区
           $(".video_l p").each(function(index){
 		    $(".video_l p").eq(index).addClass("video"+(index+1))
+		    $(".video_l p").not("video1").hide()
 		  });
           $(".videoTitle .videodiv").click(function(){
           	 $(".videoTitle .videodiv").find("span").removeClass("cur")
           	 $(this).find("span").addClass("cur")
           	 if($(this).hasClass("one")){
           	 	$(".video_l p.video1").show()
-          	 	$(".video_l p.video2").hide()
-          	 	$(".video_l p.video3").hide()
+          	 	$("p").not(".video1").hide()
 
           	 }
           	 if($(this).hasClass("two")){
-          	 	$(".video_l p.video1").hide()
           	 	$(".video_l p.video2").show()
-          	 	$(".video_l p.video3").hide()
-
+          	 	$("p").not(".video2").hide()
           	 }
           	 if($(this).hasClass("three")){
-          	 	$(".video_l p.video1").hide()
-          	 	$(".video_l p.video2").hide()
           	 	$(".video_l p.video3").show()
+          	 	$("p").not(".video3").hide()
           	 }
 
           }) //首页视频专区end
