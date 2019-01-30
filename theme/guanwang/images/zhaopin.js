08c3530fd982b4bad2585ce93d13b747bc3632f9
ducong123 <709558813@qq.com>
2019-01-30 11:26:00 +0800

    修改招聘列表

--- theme/guanwang/images/zhaopin.js
+++ theme/guanwang/images/zhaopin.js
@@ -4,9 +4,13 @@ $(function() {
        if(!$(this).parents(".toggles").find(".cont_txt").is(":hidden")){
           $(this).removeClass("icon-angle-down");
            $(this).addClass("icon-angle-up");
+           $(this).css({"color":"#036EB8"});
+           $(this).parents("ul").find("li").css({"background":"#036EB8"})
        }else{
            $(this).removeClass("icon-angle-up");
            $(this).addClass("icon-angle-down");
+           $(this).css({"color":"#2EA7E0"});
+           $(this).parents("ul").find("li").css({"background":"#2EA7E0"})
        }
     
     });
